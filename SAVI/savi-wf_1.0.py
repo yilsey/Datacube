c878e1bbbb776227ab2b93c132343b0c6e02f711
yilsey <38266709+yilsey@users.noreply.github.com>
2020-01-15 10:30:21 -0500

    Update and rename savi_wf-1.0.py to savi-wf_1.0.py

--- SAVI/savi-wf_1.0.py
+++ SAVI/savi-wf_1.0.py
@@ -3,19 +3,22 @@ import xarray as xr
 import numpy as np
 print("Indice de Vegetacion Ajustado al Suelo")
 
-#
+
 period_red = xarr0["red"].values
 period_nir = xarr0["nir"].values
 
 print("mascara")
 mask_nan=np.logical_or(np.isnan(period_nir), np.isnan(period_red))
+##Enmascara los valores de nan de las bandas seleccionadas
 period_savi1 = (period_nir-period_red)/(period_nir+period_red+0.5) 
+#Calculo de valores para el indice de vegetacion 
 period_savi = (1+0.5)*period_savi1
 period_savi[mask_nan]=np.nan
 #Hace un clip para evitar valores extremos.
 period_savi[period_savi>1]=np.nan
 period_savi[period_savi<-1]=np.nan
 
+#Asignacion de dimensiones y preparacion de salida
 ncoords=[]
 xdims =[]
 xcords={}
